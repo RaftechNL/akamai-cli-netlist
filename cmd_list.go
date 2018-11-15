ce04c49fe660edd345663592a8ebd77d065b4c72
RafPe <rafal@pieniazek.nl>
2018-11-15 15:57:51 +0100

    completed listNetLists function to v2

--- cmd_list.go
+++ cmd_list.go
@@ -12,10 +12,17 @@ func cmdlistNetLists(c *cli.Context) error {
 	return listNetLists(c)
 }
 
-// cmdlistNetList is used by cli to execute action of listing specific network list
+// cmdlistNetListId is used by cli to execute action of listing specific network list
 //
 // cmd_list
-func cmdlistNetList(c *cli.Context) error {
+func cmdlistNetListID(c *cli.Context) error {
+	return listNetList(c)
+}
+
+// cmdlistNetListName is used by cli to execute action of listing specific network list
+//
+// cmd_list
+func cmdlistNetListName(c *cli.Context) error {
 	return listNetList(c)
 }
 
@@ -24,22 +31,27 @@ func cmdlistNetList(c *cli.Context) error {
 // cmd_list
 func listNetLists(c *cli.Context) error {
 
-	listNetListOpts.TypeOflist = "IP"
-	netListsIP, _, err := apiClient.NetworkLists.ListNetworkLists(listNetListOpts)
+	// Since we are listing all we do not filter results
+	listNetListOptsv2.Search = ""
 
-	listNetListOpts.TypeOflist = "GEO"
-	netListsGEO, _, err := apiClient.NetworkLists.ListNetworkLists(listNetListOpts)
-
-	netListsResult := append(*netListsIP, *netListsGEO...)
-
-	if err != nil {
-		return err
+	// List all IP based
+	listNetListOptsv2.TypeOflist = "IP"
+	netListsIP, netlistErr := apiClient.NetworkListsv2.ListNetworkLists(listNetListOptsv2)
+	if netlistErr != nil {
+		return netlistErr
 	}
 
+	// List all GEO based
+	listNetListOptsv2.TypeOflist = "GEO"
+	netListsGEO, netlistErr := apiClient.NetworkListsv2.ListNetworkLists(listNetListOptsv2)
+	if netlistErr != nil {
+		return netlistErr
+	}
+
+	netListsResult := append(*netListsIP, *netListsGEO...)
 	common.OutputJSON(netListsResult)
 
 	return nil
-
 }
 
 // listNetLists execute client API call to get specific network list
@@ -48,13 +60,13 @@ func listNetLists(c *cli.Context) error {
 func listNetList(c *cli.Context) error {
 	common.VerifyArgumentByName(c, "id")
 
-	netList, _, err := apiClient.NetworkLists.GetNetworkList(listID, listNetListOpts)
+	// netList, _, err := apiClient.NetworkLists.GetNetworkList(listID, listNetListOpts)
 
-	if err != nil {
-		return err
-	}
+	// if err != nil {
+	// 	return err
+	// }
 
-	common.OutputJSON(netList)
+	// common.OutputJSON(netList)
 
 	return nil
 }
