832b236ba9e159224240ddb8bb0de0975df2cb26
RafPe <rafal@pieniazek.nl>
2019-11-11 14:06:45 +0100

    adds diff func for sync methods #16

---
+++ stringops.go
@@ -0,0 +1,22 @@
+package main
+
+// StringsSlicesDifference give you diff between 2 strings slices
+// Using slice1 as source and slice2 as target
+func stringsSlicesDifference(slice1 []string, slice2 []string) []string {
+	var diff []string
+	for _, s1 := range slice1 {
+		found := false
+		for _, s2 := range slice2 {
+			if s1 == s2 {
+				found = true
+				break
+			}
+		}
+		// String not found. We add it to return slice
+		if !found {
+			diff = append(diff, s1)
+		}
+	}
+
+	return diff
+}
